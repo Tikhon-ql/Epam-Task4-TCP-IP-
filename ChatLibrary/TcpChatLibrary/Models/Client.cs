cd0a8e6bbac0e8e05cc12fd90b4d3c1138437b9c
Tikhon-ql <tihkon.grek@mail.ru>
2020-08-06 09:03:53 +0300

    commit

---
+++ ChatLibrary/TcpChatLibrary/Models/Client.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpChatLibrary.Models
+{
+    public class Client
+    {
+        public IPAddress Ip { get; set; }
+        public NetworkStream stream { get; private set; }
+        TcpClient client;
+        public string Name { get; set; }
+
+        public delegate void MyEventHandler(object sender, string message);
+        public event MyEventHandler RecivedMessageFromServer;
+
+        public Client(IPAddress iP, string name)
+        {
+            client = new TcpClient();
+            Name = name;
+            Ip = iP;
+        }
+
+        //public string GetMessage()
+        //{
+        //    if (stream != null)
+        //    {
+        //        byte[] bytes = new byte[1024];
+        //        string message = "";
+        //        while (stream.DataAvailable)
+        //        {
+        //            int byteCount = stream.Read(bytes, 0, bytes.Length);
+        //            message += Encoding.Unicode.GetString(bytes, 0, byteCount);
+        //        }
+        //        return message;
+        //    }
+        //    else
+        //        throw new NullReferenceException();
+        //}
+        //public void Close()
+        //{
+        //    if (stream != null)
+        //        stream.Close();
+        //    if (client != null)
+        //        client.Close();
+        //}
+        public bool Connect(IPAddress ip, int port)
+        {
+            try
+            {
+                client.Connect(ip, port);
+                Thread reciveMessageThread = new Thread(ReciveMessage);
+                Thread joiningThread = new Thread(Joining);
+                reciveMessageThread.Start();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public void Joining()
+        {
+            string message = Name;
+            while (true)
+            {
+                SendMessage(message);
+            }
+        }
+        public void ReciveMessage()
+        {
+            if (stream != null)
+            {
+                byte[] data = new byte[1024];
+                int bytCount = stream.Read(data, 0, data.Length);
+                string message = Encoding.Unicode.GetString(data, 0, bytCount);
+                RecivedMessageFromServer?.Invoke(this, message);
+            }
+            else
+                throw new NullReferenceException();
+        }
+        private void SendMessage(string message)
+        {
+            byte[] bytes = Encoding.Unicode.GetBytes(message);
+            stream.Write(bytes, 0, bytes.Length);
+        }
+    }
---
+++ ChatLibrary/TranslatorLibrary/Models/Translator.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TranslatorLibrary.Models
+{
+    public static class Translator
+    {
+        static Dictionary<char, string> rusToEng = new Dictionary<char, string>();
+        //static Dictionary<char, char> engToRus;
+        static Translator()
+        {
+            rusToEng.Add('а', "a");
+            rusToEng.Add('б', "b");
+            rusToEng.Add('в', "v");
+            rusToEng.Add('г', "g");
+            rusToEng.Add('д', "d");
+            rusToEng.Add('е', "e");
+            rusToEng.Add('ё', "yo");
+            rusToEng.Add('ж', "zh");
+            rusToEng.Add('з', "z");
+            rusToEng.Add('и', "i");
+            rusToEng.Add('й', "j");
+            rusToEng.Add('к', "k");
+            rusToEng.Add('л', "l");
+            rusToEng.Add('м', "m");
+            rusToEng.Add('н', "n");
+            rusToEng.Add('о', "o");
+            rusToEng.Add('п', "p");
+            rusToEng.Add('р', "r");
+            rusToEng.Add('с', "s");
+            rusToEng.Add('т', "t");
+            rusToEng.Add('у', "u");
+            rusToEng.Add('ф', "f");
+            rusToEng.Add('х', "h");
+            rusToEng.Add('ц', "c");
+            rusToEng.Add('ч', "ch");
+            rusToEng.Add('ш', "sh");
+            rusToEng.Add('щ', "sch");
+            rusToEng.Add('ъ', "j");
+            rusToEng.Add('ы', "i");
+            rusToEng.Add('ь', "j");
+            rusToEng.Add('э', "e");
+            rusToEng.Add('ю', "yu");
+            rusToEng.Add('я', "ya");
+            rusToEng.Add('А', "A");
+            rusToEng.Add('Б', "B");
+            rusToEng.Add('В', "V");
+            rusToEng.Add('Г', "G");
+            rusToEng.Add('Д', "D");
+            rusToEng.Add('Е', "E");
+            rusToEng.Add('Ё', "Yo");
+            rusToEng.Add('Ж', "Zh");
+            rusToEng.Add('З', "Z");
+            rusToEng.Add('И', "I");
+            rusToEng.Add('Й', "J");
+            rusToEng.Add('К', "K");
+            rusToEng.Add('Л', "L");
+            rusToEng.Add('М', "M");
+            rusToEng.Add('Н', "N");
+            rusToEng.Add('О', "O");
+            rusToEng.Add('П', "P");
+            rusToEng.Add('Р', "R");
+            rusToEng.Add('С', "S");
+            rusToEng.Add('Т', "T");
+            rusToEng.Add('У', "U");
+            rusToEng.Add('Ф', "F");
+            rusToEng.Add('Х', "H");
+            rusToEng.Add('Ц', "C");
+            rusToEng.Add('Ч', "Ch");
+            rusToEng.Add('Ш', "Sh");
+            rusToEng.Add('Щ', "Sch");
+            rusToEng.Add('Ъ', "J");
+            rusToEng.Add('Ы', "I");
+            rusToEng.Add('Ь', "J");
+            rusToEng.Add('Э', "E");
+            rusToEng.Add('Ю', "Yu");
+            rusToEng.Add('Я', "Ya");
+        }
+        public static string Translate(string message)
+        {
+            string translateMessage = message;
+            int i = 0;
+            foreach (KeyValuePair<char, string> item in rusToEng)
+            {
+                if ((translateMessage[i] > 'a' && translateMessage[i] < 'z') || (translateMessage[i] > 'A' && translateMessage[i] < 'Z'))
+                    translateMessage = translateMessage.Replace(item.Value, item.Key.ToString());
+                else
+                    translateMessage = translateMessage.Replace(item.Key.ToString(), item.Value);
+            }
+            return translateMessage;
+        }
+    }
+}
